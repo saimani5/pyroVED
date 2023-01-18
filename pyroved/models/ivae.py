d4d786d96414330a1e87d0b5f0cf220355ca66bb
Mani <55563282+saimani5@users.noreply.github.com>
2023-01-17 22:26:07 -0500

    Added manifold_traversal to ivae.
    This method can be used to plot the traversal manifolds for cVAE.

--- pyroved/models/ivae.py
+++ pyroved/models/ivae.py
@@ -139,7 +139,8 @@ class iVAE(baseVAE):
         pyro.clear_param_store()
         # Set all torch manual seeds
         set_deterministic_mode(seed)
-
+        
+        self.data_dim = data_dim
         # Initialize the encoder network
         self.encoder_z = fcEncoderNet(
             data_dim, latent_dim + self.coord, c_dim, hidden_dim_e,
@@ -157,6 +158,7 @@ class iVAE(baseVAE):
         # Sets continuous and discrete dimensions
         self.z_dim = latent_dim + self.coord
         self.c_dim = c_dim
+        self.num_classes = c_dim
 
         # Move model parameters to appropriate device
         self.to(self.device)
@@ -307,3 +309,36 @@ class iVAE(baseVAE):
             elif self.ndim == 1:
                 plot_spect_grid(loc, d, **kwargs)
         return loc
+    
+    def manifold_traversal(self, d: int, cont_idx: int, cont_idx_fixed: int = 0,
+                           plot: bool = True, **kwargs: Union[str, int, float]
+                           ) -> torch.Tensor:
+        """
+        Latent space traversal for continuous and discrete latent variables
+        Args:
+            d: Grid size
+            cont_idx:
+                Continuous latent variable used for plotting
+                a latent manifold traversal
+            cont_idx_fixed:
+                Value which the remaining continuous latent variables are fixed at
+            plot:
+                Plots the generated manifold (Default: True)
+            kwargs:
+                Keyword arguments include custom min/max values for grid
+                boundaries passed as 'z_coord' (e.g. z_coord = [-3, 3, -3, 3]),
+                'angle' and 'shift' to condition a generative model one,
+                and plot parameters ('padding', 'pad_value', 'cmap', 'origin', 'ylim')
+        """
+        num_samples = d**2
+        disc_dim = self.num_classes
+        cont_dim = self.z_dim - self.coord
+        data_dim = self.data_dim
+        # Get continuous and discrete latent coordinates
+        samples_cont, samples_disc = generate_latent_grid_traversal(
+            d, cont_dim, disc_dim, cont_idx, cont_idx_fixed, num_samples)
+        # Pass discrete and continuous latent coordinates through a decoder
+        decoded = self.decode(samples_cont, samples_disc, **kwargs)
+        if plot:
+            plot_grid_traversal(decoded, d, data_dim, disc_dim, **kwargs)
+        return decoded
